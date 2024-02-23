304953983ea71cf7fa0f0c319ad1b101b47e6684
KHUN SOE MOE AUNG <prince.sma07@gmail.com>
2024-02-22 22:06:45 -0600

    Initialize Project

---
+++ app/src/main/java/com/khun/chemicalguessinggame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.khun.chemicalguessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
