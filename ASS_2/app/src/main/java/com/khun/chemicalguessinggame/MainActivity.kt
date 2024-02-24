8e0fcf3464b2dc88387b04079bd3fedeb83e459c
KHUN SOE MOE AUNG <prince.sma07@gmail.com>
2024-02-24 11:25:00 -0600

    Ready to submit

--- ASS_2/app/src/main/java/com/khun/chemicalguessinggame/MainActivity.kt
+++ ASS_2/app/src/main/java/com/khun/chemicalguessinggame/MainActivity.kt
@@ -2,10 +2,37 @@ package com.khun.chemicalguessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.khun.chemicalguessinggame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private val chemicals = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnAddChemical.setOnClickListener {
+            val chemicalName = binding.edtChemicalName.text.toString()
+            if (chemicalName.isNotBlank()){
+                if (chemicals.contains(chemicalName)){
+                    binding.tvResult.text = "Chemical '$chemicalName' is already available"
+                }else{
+                    chemicals.add(chemicalName)
+                }
+            }
+        }
+
+        binding.btnGuess.setOnClickListener {
+            val guessName = binding.edtGuess.text.toString()
+            if (guessName.isNotBlank() && chemicals.isNotEmpty()){
+                val randomChemical = chemicals.random()
+                if (randomChemical == guessName)
+                    binding.tvResult.text = "Congratulations! You guessed it right. It was $guessName."
+                else
+                    binding.tvResult.text = "Sorry, wrong guess. The correct answer was $randomChemical."
+            }
+        }
     }
 }
